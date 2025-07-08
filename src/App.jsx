efd29826ff8c146e08556ef04a18dc31aa53658d
Piotr Deregowski <deregowskipiotr@interia.pl>
2025-07-08 16:54:03 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
