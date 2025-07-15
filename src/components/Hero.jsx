c2f9468d0d76fc3ae0288d094eed9c92c126402a
Piotr Deregowski <deregowskipiotr@interia.pl>
2025-07-15 19:45:11 +0200

    Create Hero section with animation

---
+++ src/components/Hero.jsx
@@ -0,0 +1,121 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { SplitText } from 'gsap/all';
+import { useRef } from 'react';
+import { useMediaQuery } from 'react-responsive';
+
+const Hero = () => {
+  const videoRef = useRef(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
+  useGSAP(() => {
+    const heroSplit = new SplitText('.title', { type: 'chars, words'});
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      stagger: 0.06,
+      duration: 1.8,
+      ease: 'expo.out',
+    });
+
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      stagger: 0.06,
+      ease: 'expo.out',
+      delay: 1,
+  });
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: "#hero",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    })
+
+    .to('.right-leaf', { y:200 }, 0)
+    .to('.left-leaf', { y:-200 }, 0)
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    // Video animation timeline
+    // Create the timeline with a default duration
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    })
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      })
+    };
+    
+  }, []);
+
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">Mixology</h1>
+
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool Crisp Classic</p>
+              <p className="subtitle">
+                Sip the Spirit
+                <br />
+                of Summer
+              </p>
+            </div>
+
+            <div className='view-cocktails'>
+              <p className="subtitle">
+                Turn your home into a cocktail lounge — because every night
+                deserves something clever and tasty.
+              </p>
+              <a href='#cocktails'>View Cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className='video absolute inset-0'>
+        <video 
+          ref={videoRef}
+          src='/videos/output.mp4'
+          muted
+          playsInline
+          preload='auto'
+        />
+      </div>
+    </>
+  );
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
     navTween.fromTo("nav", { backgroundColor: "transparent" }, { 
       backgroundColor: "#00000050", 
-      backgroundFilter: "blur(15px)",
+      backgroundFilter: "blur(20px)",
       duration: 1 
     });
   })
@@ -24,13 +24,13 @@ const Navbar = () => {
         <a href="#home" className="flex items-center gap-2">
           <img src="/images/logo.png" alt="Logo" className="w-10 h-10" />
           <p>
-            <span className="italic font-thin text-indigo-400">BART</span>ender
+            SI<span className="font-thin text-indigo-400">PP</span>ER
           </p>
         </a>
 
         <ul>
           {navLinks.map((link) => (
-            <li key={link.id} className="hover:text-indigo-400 duration-500 transition-all ease-in-out">
+            <li key={link.id} className="hover:text-indigo-300 duration-500 transition-all ease-in-out">
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
